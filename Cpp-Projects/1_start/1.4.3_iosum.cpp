585986bc9853d09ca580d2a8fb1850a3316f992c
qiaozhi <qiaozhi_li@126.com>
2021-12-13 14:28:16 +0800

    2021.12.13

--- Cpp-Projects/1_start/1.4.3_iosum.cpp
+++ Cpp-Projects/1_start/1.4.3_iosum.cpp
@@ -5,16 +5,11 @@ using namespace std;
 int main()
 {
 
-    
-
-
-
-
-
-
-
-
-
-
+    int sum = 0, value = 0;
+    while (cin >> value)
+        sum += value;
+    cout << "×ÜºÍÊÇ£º" << sum << endl;
+    system("pause");
+    return 0;
 
 }

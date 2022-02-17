4a1287ad7eb965f704cb556fd038c2c57f0fca93
qiaozhi <qiaozhi_li@126.com>
2022-02-17 15:46:17 +0800

    2022.02.17

---
+++ Cpp-Projects/2_basis/unsigned2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
+int main()
+{
+    unsigned i = 40;
+    int a = 30;
+
+    cout << i - a << endl;
+    cout << a - i << endl;
+
+    system("pause");
+    return 0;
+}
